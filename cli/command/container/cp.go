5c0f503ef9a84f59913770b78c920aa1726a1aa0
Maximillian Fan Xavier <maximillianfx@gmail.com>
2021-01-14 20:03:58 -0300

    Enable quiet variable when stdout is a terminal
    Signed-off-by: Maximillian Fan Xavier <maximillianfx@gmail.com>

--- cli/command/container/cp.go
+++ cli/command/container/cp.go
@@ -102,10 +102,12 @@ func NewCopyCommand(dockerCli command.Cli) *cobra.Command {
 		},
 	}
 
+	fileInfo, _ := os.Stdin.Stat()
+
 	flags := cmd.Flags()
 	flags.BoolVarP(&opts.followLink, "follow-link", "L", false, "Always follow symbol link in SRC_PATH")
 	flags.BoolVarP(&opts.copyUIDGID, "archive", "a", false, "Archive mode (copy all uid/gid information)")
-	flags.BoolVarP(&opts.quiet, "quiet", "q", false, "Does not display copying process")
+	flags.BoolVarP(&opts.quiet, "quiet", "q", (fileInfo.Mode() & os.ModeCharDevice) == 0, "Does not display copying process")
 	return cmd
 }
 
