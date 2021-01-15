2de418e0ca5d88211c9a0a0857d86c2ac5e9fc6d
Maximillian Fan Xavier <maximillianfx@gmail.com>
2021-01-15 11:39:45 -0300

    Fix test
    Signed-off-by: Maximillian Fan Xavier <maximillianfx@gmail.com>

--- cmd/docker/docker_test.go
+++ cmd/docker/docker_test.go
@@ -16,8 +16,8 @@ import (
 
 func TestClientDebugEnabled(t *testing.T) {
 	defer debug.Disable()
-
-	tcmd := newDockerCommand(&command.DockerCli{})
+	cli, err := command.NewDockerCli()
+	tcmd := newDockerCommand(cli)
 	tcmd.SetFlag("debug", "true")
 	cmd, _, err := tcmd.HandleGlobalFlags()
 	assert.NilError(t, err)
